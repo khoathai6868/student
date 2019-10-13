365094e34c75186057b324d30f649ce2e883e72e
khoathai6868 <56308178+khoathai6868@users.noreply.github.com>
2019-10-13 19:21:30 +0700

    Add files via upload

---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    //: + Mã học sinh : kiểu dữ liệu String
+    // + Tên học sinh: Kiểu dữ liệu String
+    // + Tên môn học: Kiểu dữ liệu String
+    // + Điểm: kiểu dữ liệu float
+
+    String HoMaHocSinh, TenHocSinh, TenMonc;
+    Float Grade;
+
+
+
+
+}
---
+++ src/main/java/StudentController.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentController {
+        int x;
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        System.out.println("chose 1 to keep going, 2 to stop"+ );
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}

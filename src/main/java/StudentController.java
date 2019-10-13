c3b31de3b9f05992a9b0e1115a589c7477a28ca1
khoathai6868 <56308178+khoathai6868@users.noreply.github.com>
2019-10-13 19:39:35 +0700

    v.1.2

--- src/main/java/StudentController.java
+++ src/main/java/StudentController.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentController {
-        int x;
+        int x,y;
     public static void main(String[] args) {
         ArrayList<String> cars = new ArrayList<String>();
         System.out.println("chose 1 to keep going, 2 to stop ");
